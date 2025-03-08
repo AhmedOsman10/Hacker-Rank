5c213b2c771767fec0c3c33a18e2940723c61717
Ahmed Osman <ahmed.derwish10@gmail.com>
2025-03-09 07:06:35 +1100

    11

---
+++ Digit Frequency.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    
+    char* numbers = "0123456789";
+    char y = 0;
+    char count[10] = {};
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    char* Input = s;
+    
+
+    
+    while(y < strlen(s))
+    {
+        for(char i=0; i <= 9; i++) // loop for checking the character if it;s equal nay number from 0 to 9
+        {
+            if (*(Input + y) == numbers[i])
+            {
+                count[i]++; 
+                i = 9;
+            }
+        }
+         
+         y++;
+    }
+    
+    
+    for (int i = 0; i <= 9; i++)
+    {
+        printf("%d", count[i]);
+    }
+    return 0;
+}

918b4adcab879ad797d8b15662538b2e4123073d
Ahmed Osman <ahmed.derwish10@gmail.com>
2025-03-16 02:00:29 +1100

    15

---
+++ Printing Pattern Using Loops.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	// Complete the code to print the pattern.
+    
+    int j;
+    int k;
+    int t;
+
+    int* previous_numbers = (int*)malloc(n * sizeof(int)); // This array saves each individual number only once, e.g., 8, 7, .. , 1
+    int size = (2 * n - 1); // The lenght of a single row
+    int* numbers_saver = (int*)malloc(size * n * sizeof(int)); // This array saves all the numbers from row one until the midpoint (at n = 1).
+  	int q = 0;
+  	
+  	// This for loop prints all the numbers from the beginning until the midpoint (at n = 1)
+  	for (int i = n; 1 <= i; i--)
+  	{
+  	    previous_numbers[i-1] = i;
+  	    j = n-i;
+  	    t = (i * 2) - 1;
+  	    k = 1;
+  	    
+  	    for(int x = (n * 2) - 1; 0 < x; x--)
+  	    {
+  	        if ( 1 <= j)
+  	        {
+  	            printf("%d ", previous_numbers[n-k]);
+  	            numbers_saver[(q++)] = previous_numbers[n-k];
+  	             k++;
+  	        }
+  	        
+  	        else if( 1 <= t)
+  	        {
+  	            printf("%d ", i);
+  	            numbers_saver[q++] = i;
+  	            t--;
+  	            k = i;
+  	        }
+  	        
+  	        else
+      	    {
+      	        printf("%d ", previous_numbers[k]);
+      	       numbers_saver[q++] = previous_numbers[k];
+      	        k++;
+      	    }
+      	    
+      	    j--;
+  	    }
+  
+  	    printf("\n");
+  	}
+  	
+  	// This for loop starts printing from the first row after reaching the midpoint (at n = 1) until the end.
+  	for (int s = (((n*2)-1) * n) - ((n*2)-1); s > 0 ; s--)
+  	{
+  	    for(int m = ((n*2)-1); 0 < m; m--)
+  	    {
+  	        printf("%d ", numbers_saver[--s]);
+  	    }
+  	    printf("\n");
+  	    s++;
+  	}
+    return 0;
+}
